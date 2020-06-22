b6691e7666f3976ee0dcfe6040594ce8a2c07d4b
Ian910 <66122637+Ian910@users.noreply.github.com>
2020-06-22 16:08:29 +0800

    Add files via upload

---
+++ test6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum=0;
+	for (int i = 1; i <= 100 ; i=i+2)
+	{
+		sum += i;
+		cout << i << "+";
+	}
+	cout << "=" << sum;
+
+	 
+
+
+	system("pause");
+	return 0;
+}
