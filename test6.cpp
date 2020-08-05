1a768f0d7a7c40192031e5d057d820d170439bc0
Ian910 <66122637+Ian910@users.noreply.github.com>
2020-08-05 22:42:06 +0800

    Update test6.cpp

--- test6.cpp
+++ test6.cpp
@@ -10,10 +10,22 @@ int main()
 		cout << i << "+";
 	}
 	cout << "=" << sum;
+	system("pause");
+	return 0;
+}
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
 
-	 
-
-
+int main()
+{
+	int sum = 0;
+	for (int i = 1; i <= 100; i = i + 2)
+	{
+		sum += i;
+		printf("%d + " ,i);
+	}
+	printf("=%d", sum);
 	system("pause");
 	return 0;
 }
